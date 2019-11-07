18050771b3d04166de33f25dae27c40918fb56df
John Tucker <jtucker@appirio.com>
2019-11-07 04:50:34 -0500

    done

--- src/main.rs
+++ src/main.rs
@@ -33,9 +33,35 @@ fn do_something_crazy() {
     // println!("The value of s is: {}", s); // BORROW OF MOVED VALUE
 }
 
+fn makes_copy(some_integer: i32) { 
+    println!("The value of some_integer is: {}", some_integer); // 5
+} 
+
+fn takes_ownership(some_string: String) { 
+    println!("{}", some_string); // hello
+} 
+
+fn gives_ownership() -> String { 
+    let some_string = String::from("hello");
+    some_string
+}
+
 fn main() {
     do_something();
     do_something_else();
     do_even_more();
     do_something_crazy();
+    
+    // OWNERSHIP AND FUNCTION PARAMETERS
+    let x = 5;
+    makes_copy(x);
+    println!("The value of x is: {}", x); // 5
+
+    let s = String::from("hello");
+    takes_ownership(s);
+    // println!("The value of s is: {}", s); // BORROW OF MOVED VALUE
+
+    // OWNERSHIP AND FUNCTION RETURNS
+    let s1 = gives_ownership();
+    println!("The value of s1 is: {}", s1); // hello
 }
